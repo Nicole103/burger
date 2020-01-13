ec38345789b2f967a6dfaed3952f1b6784a030c6
Nicole Heersink <nheersink8@gmail.com>
2020-01-12 23:42:44 -0700

    creating files

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+-- Insert a set of records.
+INSERT INTO burgers (burger) VALUES ('Bacon Cheeseburger');
+INSERT INTO burgers (burger) VALUES ('Jalapeno Burger');
+INSERT INTO burgers (burger) VALUES ('Guacamole Burger');
