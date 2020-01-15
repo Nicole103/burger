8f2946b012827c62eb9a91cbac0900623ac4f697
Nicole Heersink <nheersink8@gmail.com>
2020-01-14 22:34:07 -0700

    pseudo coding routers

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -3,3 +3,14 @@ var express = require("express");
 var router = express.Router();
 
 var burger = require("../models/burger.js");
+
+//routers 
+// .get "all" router
+
+
+// .post "create" router
+
+
+// .put "update" router
+
+//Export router
