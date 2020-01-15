ac7df428c5b50e5eef7e3f192164e675231a63d5
Nicole Heersink <nheersink8@gmail.com>
2020-01-14 22:06:20 -0700

    adding orm methods all and insert

---
+++ config/orm.js
@@ -0,0 +1,59 @@
+var connection = require("../config/connection.js");
+
+//helper function to creat ?? array ie ["?", "?", "?"].toString() => "?,?,?";
+function printQuestionMarks(num) {
+    var arr = [];
+
+    for (var i = 0; i < num; i++) {
+        arr.push("?");
+    }
+
+    return arr.toString();
+}
+
+//convert object key pairs to sql syntax
+function objToSql(ob) {
+  var arr = [];
+
+    for (var key in ob) {
+      var value = ob[key];
+        if (Object.hasOwnProperty.call(ob, key)) {
+          if (typeof value === "string" && value.indexOf(" ") >= 0) {
+              value = "'" + value + "'";
+            }
+
+          arr.push(key + "=" + value);
+        }
+    }
+  return arr.toString();
+}
+var orm = {
+    all:function(tableInput, cb){
+        var queryString="SELECT * FROM "+tableInput+ ";";
+        connection.query(queryString,function(err, result){
+            if (err){
+                throw err;
+            }
+            cb(result);
+        });
+    },
+    create:function(table, cols, vals, cb){
+        var queryString = "INSERT INTO " +table;
+        queryString += " (";
+        queryString += cols.toString();
+        queryString += ") ";
+        queryString += "VALUES (";
+        queryString += printQuestionMarks(vals.length);
+        queryString += ") ";
+        
+        console.log(queryString);
+        connection.query(queryString, vals, function (err, result){
+            if(err){
+                throw err;
+            }
+
+            cb(result);
+        });
+    },
+    
+}
