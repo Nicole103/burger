98fc9462aa9bb2da76bc459f94031bf74b2a96e7
Nicole Heersink <nheersink8@gmail.com>
2020-01-18 19:36:14 -0700

    adding create function

--- public/assets/js/burgers.js
+++ public/assets/js/burgers.js
@@ -1,3 +1,20 @@
 $(function(){
-    $(".create")
+    $(".create-form").on("submit", function(event){
+        event.preventDefault();
+
+        var newBurger ={
+            burger_name: $("#burgerOrder").val().trim(),
+            devoured: 0
+        };
+
+        $.ajax("/api/burgers",{
+            type:"POST",
+            data: newBurger
+        }).then(
+            function(){
+                console.log("create new burger");
+                location.reload();
+            }
+        );
+    });
 })
