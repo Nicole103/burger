88dbf14a3d2b09322499291181d19ddf968881b9
Nicole Heersink <nheersink8@gmail.com>
2020-01-20 13:51:37 -0700

    update classes

--- public/assets/js/burgers.js
+++ public/assets/js/burgers.js
@@ -1,47 +1,47 @@
-$(function(){
+$(function () {
     //create burger 
-    $(".create-form").on("submit", function(event){
+    $(".create-form").on("submit", function (event) {
         event.preventDefault();
 
-        var newBurger ={
+        var newBurger = {
             burger_name: $("#burgerOrder").val().trim(),
             devoured: 0
         };
 
-        $.ajax("/api/burgers",{
-            type:"POST",
+        $.ajax("/api/burgers", {
+            type: "POST",
             data: newBurger
         }).then(
-            function(){
+            function () {
                 console.log("create new burger");
                 location.reload();
             }
         );
     });
     //update burger "devour burger"
-    $(".change-devour").on("click", function(event){
+    $(".change-devour").on("click", function (event) {
         var id = $(this).data("id");
         var newDevour = $(this).data("newdevour");
         var newDevourState = {
             devoured: "true"
         };
-        $.ajax("/api/burgers/" + id,{
-            type:"PUT",
+        $.ajax("/api/burgers/" + id, {
+            type: "PUT",
             data: newDevourState
         }).then(
-            function(){
+            function () {
                 console.log("changed devour to", newDevour);
                 location.reload();
             }
         );
     });
     //delete burger from devoured list
-    $(".delete-burger").on("click", function(event){
+    $(".delete-burger").on("click", function (event) {
         var id = $(this).data("id");
-        $.ajax("/api/burgers/"+ id, {
+        $.ajax("/api/burgers/" + id, {
             type: "DELETE",
         }).then(
-            function(){
+            function () {
                 console.log("delete burger", id);
                 location.reload();
             }
