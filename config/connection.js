84ecbeab6208b56c9b2c760bfa23dddbea1372b7
Nicole Heersink <nheersink8@gmail.com>
2020-01-21 19:53:14 -0700

    updating

--- config/connection.js
+++ config/connection.js
@@ -1,22 +1,20 @@
 
 var mysql = require("mysql");
+var path = require("path");
 
 require('dotenv').config();
 
-// if(process.env.JAWSDB_u) {
-//     connection = mysql.createConnection(process.env.JAWSDB_URL);
-// }else {
-
-    // process.env.SQL_PASS
-
+if(process.env.JAWSDB_URL){
+	connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else{
 var connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
     user: "root",
-    password: "",
+    password: process.env.ROOT_PASS,
     database: "burger_db"
 });
-// }
+}
 connection.connect(function (err) {
     if (err) {
         console.error("error connecting: " + err.stack);
