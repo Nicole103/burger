b69b744f62e79e584f7ea5d524c871a61ffb1386
Nicole Heersink <nheersink8@gmail.com>
2020-02-16 10:34:22 -0700

    update

--- db/schema.sql
+++ db/schema.sql
@@ -8,7 +8,7 @@ CREATE TABLE burgers (
   id INT AUTO_INCREMENT NOT NULL,
   burger_name varchar(255) NOT NULL,
   devoured BOOLEAN DEFAULT false,
-  createdAt TIMESTAMP NOT NULL,
+  createdAt DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (id)
 );
 
